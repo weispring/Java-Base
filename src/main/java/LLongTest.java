bff74caaae9f87bf66e795582b4eb86956b5f568
lixianchun <li.xianchun@vpclub.cn>
2018-12-02 11:33:02 +0800

    long hash code

--- src/main/java/LLongTest.java
+++ src/main/java/LLongTest.java
@@ -38,11 +38,31 @@ public class LLongTest {
         new Long(10).hashCode();
 
 
-
-
-
-
     }
+
+    /**
+     * 1. 四种整形包装类皆有缓存，-128 - 127，Integer 可以通过jvm参数配置 最大值大于127
+     *
+     * 在-128~127的Integer值并且以Integer x = value;的方式赋值的Integer值在进行==和equals比较时，都会返回true，
+     * 因为Java里面对处在在-128~127之间的Integer值，用的是原生数据类型int，会在内存里供重用，也就是说这之间的Integer值进行==比较时只是进行int原生数据类型的数值比较，
+     * 而超出-128~127的范围，进行==比较时是进行地址及数值比较。
+     *
+     * 2. 四种包装类型的hashCode 都和value有关
+     * 3. 四种包装类型的equals方法都是值比较
+     * 4. Number包装类和String互转
+     *
+     *
+     */
+    public void testValueCache(){
+        Integer.valueOf(1);
+        Long.valueOf(1L);
+        Byte.valueOf((byte)2);
+        Short.valueOf((short) 1);
+    }
+
+
+
+
 
     /**
      * String 固定数组，操作产生新的对象
@@ -55,12 +75,24 @@ public class LLongTest {
      */
     @Test
     public void testString(){
-        String a = "";
-        String bb = a.replace("","");
 
 
-        //bb.hashCode()
+    }
 
+
+    /**
+     * problem
+     */
+    @Test
+    public void testIntergePro(){
+
+        String bb = "".replace("","");
+        String[] a = {};
+        String.format("",a);
+
+        Integer.toHexString(1);
+        Integer.toOctalString(1);
+        Integer.toBinaryString(1);
 
     }
 }
