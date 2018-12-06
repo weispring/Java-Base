2976dc2b860c536b7d0800619fbee467ba51c05e
lixianchun <li.xianchun@vpclub.cn>
2018-12-06 08:51:41 +0800

    List

---
+++ src/main/java/lcollection/Llist.java
@@ -0,0 +1,20 @@
+package lcollection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lixianchun on 2018/12/6.
+ **/
+public class Llist {
+
+
+
+    @Test
+    public void testList(){
+        List list = new ArrayList();
+    }
+
+}
--- src/main/java/llang/LLongTest.java
+++ src/main/java/llang/LLongTest.java
@@ -3,6 +3,9 @@ package llang;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Auther: lixianchun
  * @Date: 2018/11/24 18:12
